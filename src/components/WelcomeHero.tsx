a52873b237f3be7c368d728332b6d26f67ed3195
jpjordan10 <jeanpjordans10@gmail.com>
2024-06-19 09:15:43 -0500

    test(hero): update image

--- src/components/WelcomeHero.tsx
+++ src/components/WelcomeHero.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
 import WelcomeHeroForm from "./WelcomeHeroForm";
 
 const WelcomeHero = () => (
   <section id="home" className="flex w-full justify-center">
-    <div className="w-full relative h-[50rem] z-[1] bg-center bg-cover text-center bg-hero-image 2xl:w-10/12">
-      <div className="absolute -z-[1] w-full h-full bg-custom-gray" />
-      <div className="flex flex-col items-center gap-20 mx-auto px-4 pt-36">
-        <div className="flex flex-col gap-4">
+    <div className="relative w-full h-[50rem] 2xl:w-10/12">
+      <div className="absolute inset-0 z-[-1]">
+        <Image
+          src="/banner.jpg"
+          alt="Hero Background"
+          layout="fill"
+          objectFit="cover"
+          priority
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-custom-gray" />
+      </div>
+      <div className="flex flex-col items-center gap-20 mx-auto px-4 pt-36 relative z-10">
+        <div className="flex flex-col gap-4 text-center">
           <h2 className="animate-fadeInUp text-2xl md:text-[2.5rem] xl:text-5xl text-white font-bold tracking-[2.4px] leading-tight uppercase">
             Best place to find and explore
             <br />
